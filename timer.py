ccdd4996616a81ba01017bbbf86fcaf187a5f8eb
Youwei Zhang <>
2021-05-17 15:45:42 +0000

    Add class

--- timer.py
+++ timer.py
@@ -2,20 +2,46 @@
 import sys
 import time
 
-def tomato_timer():
-  # sleep for 25 mins
-  # each counter represents 30 seconds
-  total, counter = 50, 50
-  while counter:
-    print('|', end='')
-    for i in range(total-counter):
-      print('>', end='')
-    for i in range(counter):
-      print('=', end='')
-    
-    print('|')
-    counter -= 1
-    time.sleep(3)
+class Timer():
+  PERIOD = 30
+  def __init__(self):
+    pass
+  def tomato_on(self):
+    # sleep for 25 mins
+    # each counter represents 30 seconds
+    total, counter = 50, 50
+    while counter:
+      print('|', end='')
+      for i in range(total-counter):
+        print('>', end='')
+      for i in range(counter):
+        print('=', end='')
+      print('|')
+      counter -= 1
+      time.sleep(self.PERIOD)
+  def tomato_off(self):
+    # sleep for 25 mins
+    # each counter represents 30 seconds
+    total, counter = 10, 10
+    while counter:
+      print('Rest Period |', end='')
+      for i in range(total-counter):
+        print('>', end='')
+      for i in range(counter):
+        print('=', end='')
+      print('|')
+      counter -= 1
+      time.sleep(self.PERIOD)
+
+  def tomato_cycle(self):
+    # Start from 9:00 to 17:00
+    for i in range(17-9):
+      self.tomato_on()
+      print('Tomato Timer End !!!')
+
+      self.tomato_off()
+      print('Time to work!!!')
 
 if (len(sys.argv)==1):
-  tomato_timer()
+  timer = Timer()
+  timer.tomato_cycle()
