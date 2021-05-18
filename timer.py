5fab154c65e02cc1bbfb6fe2d2e9115c8ab06ffd
Youwei Zhang <>
2021-05-18 13:36:56 +0000

    Recognize current time

--- timer.py
+++ timer.py
@@ -6,10 +6,10 @@ class Timer():
   PERIOD = 30
   def __init__(self):
     pass
-  def tomato_on(self):
+  def tomato_on(self, min=25):
     # sleep for 25 mins
     # each counter represents 30 seconds
-    total, counter = 50, 50
+    total, counter = 50, 50 - min*2
     while counter:
       print('|', end='')
       for i in range(total-counter):
@@ -33,15 +33,23 @@ class Timer():
       counter -= 1
       time.sleep(self.PERIOD)
 
-  def tomato_cycle(self):
+  def tomato_cycle(self, c_hr, c_min):
     # Start from 9:00 to 17:00
-    for i in range(17-9):
-      self.tomato_on()
+    period = (17-c_hr)*2
+    if c_min >= 30:
+      c_min -= 30
+      period += 1
+    
+    for i in range(period):
+      self.tomato_on(c_min)
+      c_min = 25
       print('Tomato Timer End !!!')
-
       self.tomato_off()
       print('Time to work!!!')
 
 if (len(sys.argv)==1):
+  c_hr = int(time.strftime("%H",time.gmtime()))-4
+  c_min = int(time.strftime("%M",time.gmtime()))
+
   timer = Timer()
-  timer.tomato_cycle()
+  timer.tomato_cycle(c_hr, c_min)
